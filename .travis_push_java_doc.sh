2d7799c78afd14bb8390dd91b5e0cfac796c6844
Amin F <aminfaez@mail.upb.de>
2019-02-02 10:11:40 +0100

    Fixed travis java doc push script

--- .travis_push_java_doc.sh
+++ .travis_push_java_doc.sh
@@ -6,14 +6,17 @@ setup_git() {
 }
 
 commit_website_files() {
-  git checkout -b gh-pages
-  git add -A
+  git checkout -b javadoc
+  git add ./\*.html
+  git add ./\*.css
+  git add ./\*.js
+  git add ./\*package-list
   git commit --message "Travis built Javadoc"
 }
 
 upload_files() {
-  git remote add origin-pages https://${GH_TOKEN}@github.com/aminfa/SEDE.git > /dev/null 2>&1
-  git push --quiet --set-upstream origin-pages gh-pages 
+  git remote add origin-pages https://${GH_TOKEN}@github.com/aminfa/AILibs.git > /dev/null 2>&1
+  git push --quiet --set-upstream origin-pages javadoc 
 }
 
 setup_git
