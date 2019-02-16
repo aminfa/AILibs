952e36a14c5fad3d69fe8d72133b83a6e4fa7816
Amin F <aminfaez@mail.upb.de>
2019-02-16 01:04:13 +0100

    Fix to commit script

--- .git_java_doc_functions.sh
+++ .git_java_doc_functions.sh
@@ -3,6 +3,13 @@
 setup_git() {
   git config --global user.email "travis@travis-ci.org"
   git config --global user.name "Travis CI"
+  git remote add origin-down https://github.com/aminfa/AILibs.git
+  git remote add origin-up https://${GH_TOKEN}@github.com/aminfa/AILibs.git > /dev/null 2>&1
+}
+
+pull_git() {
+  git remote update
+  git checkout --track origin-down/javadoc
 }
 
 commit_website_files() {
@@ -17,10 +24,5 @@ commit_website_files() {
 }
 
 upload_files() {
-  git remote add origin-up https://${GH_TOKEN}@github.com/aminfa/AILibs.git > /dev/null 2>&1
   git push --quiet --set-upstream origin-up javadoc 
 }
-
-setup_git
-commit_website_files
-upload_files
